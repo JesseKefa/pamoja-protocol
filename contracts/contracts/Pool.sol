33f25c42e15a4dcf58570fb35ddb47802badae70
Jesse Kefa <jessekefa86@gmail.com>
2026-06-27 13:36:28 +0300

    feat: implement Pool contract with constructor and state initialization.

---
+++ contracts/contracts/Pool.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Pool {
+    uint256 public id;
+    string public name;
+    string public description;
+    address public creator;
+}
+
+constructor (
+    uint256 _id,
+    string memory _name,
+    string memory _description,
+    address _creator;
+) {
+    id = _id;
+    name = _name;
+    description = _description;
+    creator = _creator;
+}
--- contracts/contracts/PoolFactory.sol
+++ contracts/contracts/PoolFactory.sol
@@ -1,3 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "./Pool.sol";
 contract PoolFactory {
-    
-}
+
+    struct PoolInfo {
+        uint256 id;
+        string name;
+        string description;
+        address creator;
+        address poolAddress;
+        uint256 createdAt;
+        bool isActive;
+     }
+
+    PoolInfo[] public pools;
+
+    uint256 private nextPoolId = 1;
+
+    function createPool(
+        string memory name,
+        string memory description
+    ) public {
+        uint256 poolId = nextPoolId;
+    }
+
+    Pool newPool = new Pool(
+        poolId,
+        name,
+        description,
+        msg.sender
+    )
+
+
+    }
