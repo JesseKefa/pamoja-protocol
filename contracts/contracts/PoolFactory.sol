87d1aab4fdd6964ac2ad8f820d2e9c693c5c683f
Jesse Kefa <jessekefa86@gmail.com>
2026-06-30 20:20:51 +0300

    feat: implement community creation in PoolFactory.

--- contracts/contracts/PoolFactory.sol
+++ contracts/contracts/PoolFactory.sol
@@ -17,20 +17,47 @@ contract PoolFactory {
     PoolInfo[] public pools;
 
     uint256 private nextPoolId = 1;
+    
+    event PoolCreated(
+        uint256 indexed id,
+        address indexed creator,
+        address poolAddress,
+        string name
+    );
 
     function createPool(
-        string memory name,
-        string memory description
+    string memory name,
+    string memory description
     ) public {
         uint256 poolId = nextPoolId;
-    }
 
     Pool newPool = new Pool(
         poolId,
         name,
         description,
         msg.sender
-    )
+    );
+
+    pools.push(
+        PoolInfo({
+            id: poolId,
+            name: name,
+            description: description,
+            creator: msg.sender,
+            poolAddress: address(newPool),
+            createdAt: block.timestamp,
+            isActive: true
+    })
+    );
+
+    nextPoolId++;
+
+    emit PoolCreated(
+        poolId,
+        msg.sender,
+        address(newPool),
+        name
+    );
 
 
     }
